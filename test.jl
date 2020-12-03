2e3b54dfaa805348113fd1a525dd5e236da51b9e
arthurolga <arthur_olga@hotmail.com>
2020-12-03 18:30:54 -0300

    v2.4

--- test.jl
+++ test.jl
@@ -1,24 +1,3 @@
-local a::Bool
-local i::Int
-
-a = true
-b = false
-
-n = readline()
-i = 0
-while i < (n+1)
-    i = i+1
-    
-    value = i / 2
-    
-    if (value*2 )== i
-        println("- "*i*" PAR")
-    else
-        println("- "*i*" IMPAR")
-    end
-
+if(true)
+    println( 1)
 end
-
-
-    
-
